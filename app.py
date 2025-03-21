d7491a1693f0664512c7bf957933aecff93c40cc
Yaroslav <61938366+yarov3so@users.noreply.github.com>
2025-03-20 21:10:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -101,5 +101,6 @@ elif len(modes)==1:
 else:
     st.markdown(f"Your data set has the following modes: {modes_str}")
 
+st.text("")
 st.markdown("""*See my other [Math Help Tools](https://mathh3lptools.streamlit.app)*   
 <a href="https://www.buymeacoffee.com/yarov3so" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style="width: 9em; height: auto; padding-top: 1em;" ></a>""",unsafe_allow_html=True)
